c28b3b1b54aeb816469cd6b0dc42cd433fc3f33d
Mohsin Ali <mohammadmohsin@gmail.com>
2015-06-08 21:21:37 +0500

    Devise Views and registration method for js.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        # respond_with resource, location: after_sign_up_path_for(resource)
+        respond_to :html, :js
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        # respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        respond_to :html, :js
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      # respond_with resource
+      respond_to :html, :js
+    end
+  end
 
   # GET /resource/edit
   # def edit
