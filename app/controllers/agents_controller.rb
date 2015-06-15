cc39257fa0fed3023971680f422d71af39e2d331
Mohsin Ali <mohammadmohsin@gmail.com>
2015-06-15 19:41:16 +0500

    Search filters in left side bar.

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,6 +1,6 @@
 class AgentsController < ApplicationController
 	def index
     @q = User.with_role(:agent).ransack(params[:q])
-    @agents = @q.result.page(params[:page])
+    @agents = @q.result(distinct: true).includes(:user_cities, :user_services).page(params[:page])
   end
 end
