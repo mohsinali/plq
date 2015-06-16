be76e072a5ada698a84cd5c4df8fcb845403348e
Mohsin Ali <mohammadmohsin@gmail.com>
2015-06-16 13:04:47 +0500

    Search filters with one form.

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -2,5 +2,7 @@ class AgentsController < ApplicationController
 	def index
     @q = User.with_role(:agent).ransack(params[:q])
     @agents = @q.result(distinct: true).includes(:user_cities, :user_services).page(params[:page])
+
+    respond_to :html, :js
   end
 end
