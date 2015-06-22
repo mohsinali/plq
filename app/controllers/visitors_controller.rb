5194702cfd1672bac7f598ee70cedcc425e980f9
ammarshah <ammarshah123@gmail.com>
2015-06-22 17:05:30 +0500

    Seach agent from home.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,7 @@ class VisitorsController < ApplicationController
   layout 'home'
 
   def index
+  	@q = User.with_role(:agent).ransack(params[:q])
     # file = "/home/mohsin/Project_Docs/PLQ/iso3166.csv"
     # CSV.foreach(file) do |row|
     #   # binding.pry
