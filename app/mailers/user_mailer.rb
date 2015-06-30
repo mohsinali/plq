f2690dd59b077c4118c2312fe3bea14123fc00fa
ammarshah <ammarshah123@gmail.com>
2015-06-30 18:40:52 +0500

    Show unapproved providers in admin dashboard.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def notify_admin user
   	@user = user
-  	mail(to: "Kerwin Alabi <ammarshah123@gmail.com>", subject: 'New user just signed up!')
+  	mail(to: "Kerwin Alabi <kerwin@platformiv.com>", subject: 'New user just signed up!')
   end
 
   def request_for_membership form_fields
