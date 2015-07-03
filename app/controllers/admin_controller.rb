058b3fc4f953619cd276859b252e8fa385303d9c
ammarshah <ammarshah123@gmail.com>
2015-07-03 18:42:46 +0500

    Admin can add providers.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,7 +13,35 @@ class AdminController < ApplicationController
 
   def add_provider
     if request.post?
-      binding.pry
+      user = User.new(email: params[:user][:email], password: params[:user][:password], name: params[:user][:name], location: params[:user][:location], tier: params[:user][:tier], social_links: params[:user][:social_links], country: params[:user][:country], description: params[:user][:description], approved: true)
+      if user.save
+        ## #####################################
+        ## Assign a role to newly created user
+        user.add_role params[:role]
+        
+        # Add Selected services
+        params[:user_service].each do |service|
+          UserService.create(name: service, user_id: user.id)
+        end
+
+        # Add Selected cities
+        params[:user_cities].each do |city|
+          UserCity.create(name: city, user_id: user.id)
+        end
+        
+        ## ########################
+        ## Upload Profile Image      
+        user.image = params[:user][:image]
+
+        ## #####################################
+        ## Assign interests
+        user.interest_list.add(params[:interests], parse: true)
+        user.save!
+        
+        flash[:notice] = "#{params[:role].humanize} added successfully."
+
+        redirect_to admin_index_path()
+      end
     end
   end
 
