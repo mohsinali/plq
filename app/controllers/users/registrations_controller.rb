5aa083b5517773b5f587ea33661bb90740a84761
ammarshah <ammarshah123@gmail.com>
2015-07-03 13:13:36 +0500

    Change login details in My Profile.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
-  before_filter :configure_account_update_params, only: [:update]
+  # before_filter :configure_account_update_params, only: [:update]
   
   # GET /resource/sign_up
   # def new
@@ -106,11 +106,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name, :location, {:user_service_ids => []}, {:user_city_ids => []}, :tier, :country, :social_links, :description, :image, :email, :password, :password_confirmation, :current_password)
-    end
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.for(:account_update) do |u|
+  #     u.permit(:name, :location, {:user_service_ids => []}, {:user_city_ids => []}, :tier, :country, :social_links, :description, :image, :email, :password, :password_confirmation, :current_password)
+  #   end
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
