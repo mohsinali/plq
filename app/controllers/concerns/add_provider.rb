6b5121773f190a6dc80205b81d5867517f6a7edf
ammarshah <ammarshah123@gmail.com>
2015-07-28 21:49:03 +0500

    Added Experience and Languages field in admin add_provider form.

--- app/controllers/concerns/add_provider.rb
+++ app/controllers/concerns/add_provider.rb
@@ -2,7 +2,7 @@ module AddProvider
   extend ActiveSupport::Concern
   
   def create_user
-    user = User.new(email: params[:user][:email], password: params[:user][:password], name: params[:user][:name], location: params[:user][:location], tier: params[:user][:tier], social_links: params[:user][:social_links], country: params[:user][:country], description: params[:user][:description], approved: true, manager_id: params[:manager_id])
+    user = User.new(email: params[:user][:email], password: params[:user][:password], name: params[:user][:name], location: params[:user][:location], tier: params[:user][:tier], social_links: params[:user][:social_links], country: params[:user][:country], description: params[:user][:description], approved: true, manager_id: params[:manager_id], experience: params[:user][:experience])
     if user.save
       ## #####################################
       ## Assign a role to newly created user
@@ -17,6 +17,10 @@ module AddProvider
       params[:user_cities].each do |city|
         UserCity.create(name: city, user_id: user.id)
       end
+
+      # Add selected languages
+      languages = Language.where(:id => params[:user_languages])
+      user.languages << languages
       
       ## ########################
       ## Upload Profile Image      
